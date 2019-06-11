9ca8d553f8ccf490a806ef02c36bb206d9dad512
Karan Newatia <kn348@cornell.edu>
2019-06-11 16:32:17 -0400

    apply transaction

---
+++ src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/TransactionTuple.java
@@ -0,0 +1,11 @@
+//package com.vegvisir.app.tasklist;
+//
+//public class TransactionTuple {
+//    public TransactionID transaction;
+//    public int transactionType; //0 is remove, 1 is add
+//
+//    public TransactionTuple(TransactionID tx_id, int tx_type) {
+//        transaction = tx_id;
+//        transactionType = tx_type;
+//    }
+//}
--- src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/VegvisirApplicationDelegator.java
+++ src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/VegvisirApplicationDelegator.java
@@ -1,69 +1,80 @@
-package com.vegvisir.app.tasklist;
-
-//import com.vegvisir.application.VegVisirApplicationDelegator;
-
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
-
-
-/**
- * Ideally, all applications should implement this interface.
- */
-public class VegvisirApplicationDelegatorImpl implements VegvisirApplicationDelegator {
-
-
-    /**
-     * Vegvisir will call this function to init and run application.
-     * @param instance a underlying Vegvisir instance for application use.
-     */
-    public void init(VegvisirInstance instance) {
-
-    }
-
-
-    /**
-     * An application implemented function. This function will get called whenever a new transaction
-     * subscribed by this application arrives.
-     * @param topics topics that this transaction is created for.
-     * @param payload application specific data.
-     * @param tx_id A unique identifier for the transaction.
-     * @param deps which transactions this transaction depends on.
-     */
-    public void applyTransaction(
-            Set<String> topics,
-            byte[] payload,
-            TransactionID tx_id,
-            Set<TransactionID> deps) {
-
-        if (tx_id.getDeviceID())
-
-        String item;
-        Set updatedSet;
-        Set prevSets = MainActivity.dependencySets.get(item);
-
-        Iterator<TransactionID> itr = prevSets.iterator();
-        while(itr.hasNext()){
-            TransactionID x = ((Iterator) itr).next();
-            if (!deps.contains(x)) {
-                updatedSet.add(x);
-            }
-        }
-
-        updatedSet.add(tx_id);
-        MainActivity.dependencySets.put(item, updatedSet);
-
-        MainActivity.latestTransactions.put()
-
-
-
-
-
-
-
-
-
-
-
-    }
-}
+//package com.vegvisir.app.tasklist;
+//
+////import com.vegvisir.application.VegVisirApplicationDelegator;
+//
+//import java.util.HashMap;
+//import java.util.Iterator;
+//import java.util.Set;
+//
+//
+///**
+// * Ideally, all applications should implement this interface.
+// */
+//public class VegvisirApplicationDelegatorImpl implements VegvisirApplicationDelegator {
+//
+//
+//    /**
+//     * Vegvisir will call this function to init and run application.
+//     * @param instance a underlying Vegvisir instance for application use.
+//     */
+//    public void init(VegvisirInstance instance) {
+//
+//    }
+//
+//
+//    /**
+//     * An application implemented function. This function will get called whenever a new transaction
+//     * subscribed by this application arrives.
+//     * @param topics topics that this transaction is created for.
+//     * @param payload application specific data.
+//     * @param tx_id A unique identifier for the transaction.
+//     * @param deps which transactions this transaction depends on.
+//     */
+//    public void applyTransaction(
+//            Set<String> topics,
+//            byte[] payload,
+//            TransactionID tx_id,
+//            Set<TransactionID> deps) {
+//
+//        String item;
+//        int transactionType;
+//
+//        if (tx_id.getDeviceID().parseInt() >  MainActivity.latestTransactions.get(item).getDeviceID().parseInt()){
+//
+//            Set updatedSet;
+//            Set prevSets = MainActivity.dependencySets.get(item);
+//
+//            Iterator<TransactionID> itr = prevSets.iterator();
+//            while(itr.hasNext()){
+//                TransactionTuple x = ((Iterator) itr).next();
+//                if (!deps.contains(x.transaction)) {
+//                    updatedSet.add(x);
+//                }
+//            }
+//            transactionTuple t = new TransactionTuple(tx_id, transactionType);
+//            updatedSet.add(t);
+//            MainActivity.dependencySets.put(item, updatedSet);
+//
+//            MainActivity.latestTransactions.put(tx_id.getDeviceID(), tx_id);
+//
+//            Iterator<TransactionID> it = updatedSet.iterator();
+//            boolean flag = false;
+//            while(it.hasNext()){
+//                TransactionTuple x = ((Iterator) it).next();
+//                if (x.transactionType == 0) { //0 = remove
+//                    // remove item from array in MainActivity
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//
+//            if (!flag){
+//                //add item to array in MainActivity
+//            }
+//
+//
+//        }
+//
+//
+//    }
+//}
