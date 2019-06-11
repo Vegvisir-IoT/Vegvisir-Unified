99e18a00eac2664ee88388883557bdbe0358bb73
aniroodh-ravikumar <ar765@cornell.edu>
2019-06-11 09:19:51 -0400

    Basic annotations on map

---
+++ src/apps/AnnotativeMap/app/src/main/java/com/vegvisir/app/annotativemap/PictureTagView.java
@@ -0,0 +1,122 @@
+package com.vegvisir.app.annotativemap;
+
+/**
+ * Created by jiangyi on 11/07/2017.
+ */
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PictureTagView extends RelativeLayout{
+    private Context context;
+    public TextView tvPictureTagLabel;
+    private View loTag;
+    private String time;
+    public enum Status{Normal,Edit,Del}
+    public enum Direction{Left,Right}
+    private Direction direction = Direction.Left;
+    private InputMethodManager imm;
+    private static final int ViewWidth = 80;
+    private static final int ViewHeight = 50;
+
+    public PictureTagView(Context context,Direction direction) {
+        super(context);
+        this.context = context;
+        this.direction = direction;
+        initViews();
+        init();
+    }
+
+    /** 初始化视图 **/
+    protected void initViews(){
+        LayoutInflater.from(context).inflate(R.layout.picturetagview, this,true);
+        tvPictureTagLabel = (TextView) findViewById(R.id.tvPictureTagLabel);
+        loTag = findViewById(R.id.loTag);
+    }
+    /** 初始化 **/
+    protected void init(){
+        imm = (InputMethodManager)context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        directionChange();
+    }
+
+
+    public void setAnnotation(String anno) {
+        tvPictureTagLabel.setText(anno);
+    }
+
+
+    public void setStatus(Status status){
+        switch(status){
+            case Normal:
+                tvPictureTagLabel.setVisibility(View.VISIBLE);
+//                tvPictureTagLabel.setText(etPictureTagLabel.getText());
+                //隐藏键盘
+//                imm.hideSoftInputFromWindow(etPictureTagLabel.getWindowToken() , 0);
+                break;
+            case Edit:
+                tvPictureTagLabel.setVisibility(View.GONE);
+
+                Intent intent = new Intent();
+                intent.setClass(context,add_annotation.class);
+                intent.putExtra("CUR_ANNO",tvPictureTagLabel.getText());
+                // 把已经添加的tag发送给add_annotation
+                time = getTime();
+                intent.putExtra("TIME",time);
+                ((Activity)context).startActivityForResult(intent,0);
+
+                //弹出键盘
+//                imm.toggleSoftInput(0, InputMethodManager.SHOW_FORCED);
+                break;
+            case Del:
+                tvPictureTagLabel.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        View parent = (View) getParent();
+        int halfParentW = (int) (parent.getWidth()*0.5);
+        int center = (int) (l + (this.getWidth()*0.5));
+        if(center<=halfParentW){
+            direction = Direction.Left;
+        }
+        else{
+            direction = Direction.Right;
+        }
+        directionChange();
+    }
+    private void directionChange(){
+        switch(direction){
+            case Left:
+                loTag.setBackgroundResource(R.drawable.bg_picturetagview_tagview_left);
+                break;
+            case Right:
+                loTag.setBackgroundResource(R.drawable.bg_picturetagview_tagview_right);
+                break;
+        }
+    }
+    public static int getViewWidth(){
+        return ViewWidth;
+    }
+    public static int getViewHeight(){
+        return ViewHeight;
+    }
+
+    public String getTime() {
+        long time = System.currentTimeMillis();
+        SimpleDateFormat time_format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date d1 = new Date(time);
+        String t1 = time_format.format(d1);
+        return t1;
+    }
+}
---
+++ src/apps/AnnotativeMap/app/src/main/java/com/vegvisir/app/annotativemap/picture.java
@@ -0,0 +1,118 @@
+package com.vegvisir.app.annotativemap;
+
+/**
+ * Created by jiangyi on 09/07/2017.
+ */
+
+import com.vegvisir.app.annotativemap.PictureTagView.Status;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class picture extends AppCompatActivity implements View.OnClickListener{
+    private Button send = null;
+    public int cur_pic_number = 0;
+    final String ANNOTATION = "Annotation";
+    final String TIME = "CURRENT";
+    final String DEL = "Delete";
+    String anno = "";
+    String cur_time = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("Before click","2.25");
+        super.onCreate(savedInstanceState);
+        Log.i("Before click","2.5");
+        setContentView(R.layout.pic);
+        Log.i("Before click","3");
+        PictureTagLayout image = findViewById(R.id.image);
+        Log.i("Before click","4");
+        image.setBackgroundResource(R.drawable.example_map);
+        Log.i("Before click","5");
+        image.load(); //read /sdcard/info.txt and init the subviews(annotations)
+//        image.write();
+
+        send = findViewById(R.id.send);
+        send.setOnClickListener(this);
+        Log.i("Before click","2");
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.send:
+                Intent intent = new Intent(picture.this, send.class);
+                PictureTagLayout image = findViewById(R.id.image);
+                String content = image.message();
+                image.write(content);
+                intent.putExtra("CONTENT", content);
+                startActivityForResult(intent,0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent annotation) {
+        if (resultCode == 0) { //Done
+            anno = annotation.getStringExtra(ANNOTATION);
+            cur_time = annotation.getStringExtra(TIME);
+            if (!TextUtils.isEmpty(anno)) {
+                PictureTagLayout image = findViewById(R.id.image);
+                image.setStatus(Status.Normal,anno);
+            }
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+        else if (resultCode == 1) { //Del
+            String del = annotation.getStringExtra(DEL);
+            PictureTagLayout image = findViewById(R.id.image);
+            image.setStatus(Status.Del,del);
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+        else if (resultCode == 2) { //Send Done
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+    }
+
+    public void init() {
+        InputStream is = picture.this.getClass().getClassLoader().
+                getResourceAsStream("assets/" + "info.json");
+        InputStreamReader streamReader = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(streamReader);
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            reader.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            JSONObject info = new JSONObject(stringBuilder.toString());
+            JSONObject anno_array = info.getJSONObject("ANNOTATION");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
