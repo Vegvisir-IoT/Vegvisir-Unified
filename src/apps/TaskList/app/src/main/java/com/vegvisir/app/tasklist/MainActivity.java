722de98d9d05807efec4185c53791bc8fd57110a
Karan Newatia <kn348@cornell.edu>
2019-06-11 10:58:44 -0400

    changed remove UI

--- src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/MainActivity.java
+++ src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/MainActivity.java
@@ -1,11 +1,15 @@
 package com.vegvisir.app.tasklist;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     private ListView mTaskList;
     private EditText mItemEdit;
     private Button mAddButton;
-    private Button mRemoveButton;
     private ArrayAdapter<String> mAdapter;
 
     @Override
@@ -27,9 +30,17 @@ public class MainActivity extends AppCompatActivity {
         mTaskList = (ListView) findViewById(R.id.task_listView);
         mItemEdit = (EditText) findViewById(R.id.item_editText);
         mAddButton = (Button) findViewById(R.id.add_button);
-        mRemoveButton = (Button) findViewById(R.id.remove_button);
 
-        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TextView text = (TextView) view.findViewById(android.R.id.text1);
+                text.setTextColor(Color.RED);
+                return view;
+            }
+        };
+
         mTaskList.setAdapter(mAdapter);
 
         mAddButton.setOnClickListener(new View.OnClickListener() {
@@ -42,17 +53,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mRemoveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String item = mItemEdit.getText().toString();
-                mAdapter.remove(item);
-                mAdapter.notifyDataSetChanged();
-                mItemEdit.setText("");
-            }
-        });
+//        mTaskList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                mAdapter.remove(mAdapter.getItem(position));
+//                // Refresh the adapter
+//                mAdapter.notifyDataSetChanged();
+//
+//            }
+//        });
+
+        mTaskList.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        // Remove the item within array at position
+                        mAdapter.remove(mAdapter.getItem(pos));
+                        // Refresh the adapter
+                        mAdapter.notifyDataSetChanged();
+                        // Return true consumes the long click event (marks it handled)
+                        return true;
+                    }
+
+                });
 
     }
+
 
 //    private VirtualVegvisirInstance loadVirtualVegvisirInstance() {
 //        return VirtualVegvisirInstance.getInstance();
