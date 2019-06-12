d580463022354dfcb30f6b1037622321794b9d6e
Danny Adams <da469@cornell.edu>
2019-06-12 18:11:13 -0400

    Issue #2: Skeleton for test development.

---
+++ src/pub_sub/java/Vegvisir-PubSub/vegvisirPubSub/src/test/java/com/vegvisir/pub_sub/VirtualVegvisirInstanceTest.java
@@ -0,0 +1,16 @@
+package com.vegvisir.pub_sub;
+
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+//import static org.junit.jupiter.api.Assertions.*;
+
+class VirtualVegvisirInstanceTest {
+    @Test
+     void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
