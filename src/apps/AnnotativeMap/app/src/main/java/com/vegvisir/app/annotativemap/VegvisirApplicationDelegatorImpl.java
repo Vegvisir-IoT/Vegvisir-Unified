99495831e0d74534c6660d1247ef4099dab8b318
aniroodh-ravikumar <ar765@cornell.edu>
2019-06-12 12:02:55 -0400

    Trying to checkout back to annotative-map

---
+++ src/apps/AnnotativeMap/app/src/main/java/com/vegvisir/app/annotativemap/VegvisirApplicationDelegatorImpl.java
@@ -0,0 +1,37 @@
+//import com.vegvisir.core.datatype.proto.Block;
+package com.vegvisir.app.annotativemap;
+
+import java.util.Set;
+
+import com.vegvisir.pub_sub.*;
+
+/**
+ * Ideally, all applications should implement this interface.
+ */
+public class VegvisirApplicationDelegatorImpl implements VegvisirApplicationDelegator{
+
+    /**
+     * Vegvisir will call this function to init and run application.
+     * @param instance a underlying Vegvisir instance for application use.
+     */
+    public void init(VegvisirInstance instance){
+
+    }
+
+
+    /**
+     * An application implemented function. This function will get called whenever a new transaction
+     * subscribed by this application arrives.
+     * @param topics topics that this transaction is created for.
+     * @param payload application specific data.
+     * @param tx_id A unique identifier for the transaction.
+     * @param deps which transactions this transaction depends on.
+     */
+    public void applyTransaction(
+            Set<String> topics,
+            byte[] payload,
+            TransactionID tx_id,
+            Set<TransactionID> deps) {
+
+    }
+}
---
+++ src/apps/AnnotativeMap/app/src/main/java/com/vegvisir/app/annotativemap/picture.java
@@ -0,0 +1,125 @@
+package com.vegvisir.app.annotativemap;
+
+/**
+ * Created by jiangyi on 09/07/2017.
+ */
+
+import com.vegvisir.app.annotativemap.PictureTagView.Status;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class picture extends AppCompatActivity implements View.OnClickListener{
+    private Button send = null;
+    public int cur_pic_number = 0;
+    final String ANNOTATION = "Annotation";
+    final String TIME = "CURRENT";
+    final String DEL = "Delete";
+    String anno = "";
+    String cur_time = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pic);
+        PictureTagLayout image = findViewById(R.id.image);
+        image.setBackgroundResource(R.drawable.example_map);
+        image.load(); //read /sdcard/info.txt and init the subviews(annotations)
+//        image.write();
+
+        send = findViewById(R.id.send);
+        send.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.send:
+                Intent intent = new Intent(picture.this, send.class);
+                PictureTagLayout image = findViewById(R.id.image);
+                String content = image.message();
+                image.write(content);
+                intent.putExtra("CONTENT", content);
+                startActivityForResult(intent,0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent annotation) {
+        if (resultCode == 0) { //Done
+            anno = annotation.getStringExtra(ANNOTATION);
+            cur_time = annotation.getStringExtra(TIME);
+            if (!TextUtils.isEmpty(anno)) {
+                PictureTagLayout image = findViewById(R.id.image);
+                image.setStatus(Status.Normal,anno);
+            }
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+        else if (resultCode == 1) { //Del
+            String del = annotation.getStringExtra(DEL);
+            PictureTagLayout image = findViewById(R.id.image);
+            image.setStatus(Status.Del,del);
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+        else if (resultCode == 2) { //Send Done
+            super.onActivityResult(requestCode,resultCode,annotation);
+        }
+    }
+
+    public void init() {
+        InputStream is = picture.this.getClass().getClassLoader().
+                getResourceAsStream("assets/" + "info.json");
+        InputStreamReader streamReader = new InputStreamReader(is);
+        BufferedReader reader = new BufferedReader(streamReader);
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            reader.close();
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            JSONObject info = new JSONObject(stringBuilder.toString());
+            JSONObject anno_array = info.getJSONObject("ANNOTATION");
+            JSONArray cur_pic_anno_array = anno_array.getJSONArray(cur_pic_number+"");
+            if (cur_pic_anno_array.length()==0);
+            else {
+                for (int i = 0;i < cur_pic_anno_array.length(); i++) {
+                    JSONObject temp = cur_pic_anno_array.getJSONObject(i);
+                    int x = temp.getInt("x");
+                    int y = temp.getInt("y");
+                    String a = temp.getString("anno");
+
+                    PictureTagLayout image = (PictureTagLayout) findViewById(R.id.image);
+                    View view = image.addItem(x,y);
+                    ((PictureTagView)view).setAnnotation(a);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
