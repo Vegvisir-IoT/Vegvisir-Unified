cffa00ac47711cd310520222cfaa84419b1abca3
Danny Adams <da469@cornell.edu>
2019-06-12 23:10:42 -0400

    Issue #3: Test suite updated.

--- src/pub_sub/java/Vegvisir-PubSub/vegvisirPubSub/src/test/java/com/vegvisir/pub_sub/VirtualVegvisirInstanceTest.java
+++ src/pub_sub/java/Vegvisir-PubSub/vegvisirPubSub/src/test/java/com/vegvisir/pub_sub/VirtualVegvisirInstanceTest.java
@@ -4,8 +4,7 @@ package com.vegvisir.pub_sub;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-//import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class VirtualVegvisirInstanceTest {
     @Test
