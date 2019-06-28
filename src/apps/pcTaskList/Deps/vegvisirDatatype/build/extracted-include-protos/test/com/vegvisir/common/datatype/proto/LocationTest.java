ed74c3e703e64d6dabec75f0a447f7620a41b02a
Danny Adams <da469@cornell.edu>
2019-06-27 20:28:54 -0400

    Added dependencies

---
+++ src/apps/pcTaskList/Deps/vegvisirDatatype/build/extracted-include-protos/test/com/vegvisir/common/datatype/proto/LocationTest.java
@@ -0,0 +1,27 @@
+package com.vegvisir.common.datatype.proto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+
+    @org.junit.jupiter.api.Test
+    void getLongitude() {
+        Location myLocation =  Location.newBuilder().build();
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLatitude() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAltitude() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasTimestamp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTimestamp() {
+    }
+}
