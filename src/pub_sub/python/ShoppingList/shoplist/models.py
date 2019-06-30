2e68d5593c67e1841a413f2cc6bb325046795f41
Raheel Yanful <raheel@Raheel-Yanfuls-MacBook-Pro.local>
2019-06-29 20:57:43 -0400

    started model and basic html

--- src/pub_sub/python/ShoppingList/shoplist/models.py
+++ src/pub_sub/python/ShoppingList/shoplist/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Item:
+    name : str
+    isOn : bool
+
+class Transaction:
+    TransactionID : int
+    payload : str
+
+class Person:
+    name: str
+    deviceID : str
+    transactionHeight : int
+
+class ShoppingList:
+    items : [Item]
+    people : [Person]
+
+
+
--- src/pub_sub/python/ShoppingList/shoplist/views.py
+++ src/pub_sub/python/ShoppingList/shoplist/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
+from .models import Item, Person, ShoppingList
+
+# Create your views here.
 def index(request):
-    return HttpResponse("Shopping List")
+
+    item1 = Item()
+    item1.name = "apples"
+    item1.isOn = True
+
+    item2 = Item()
+    item2.name = "bananas"
+    item2.isOn = True
+
+    user1 = Person()
+    user1.name = "Me"
+    user1.deviceID = "123"
+    user1.transactionHeight = 1
+
+    shoplist = ShoppingList()
+    shoplist.items = [item1, item2]
+    shoplist.people = [user1]
+    
+    return render(request, 'index.html', {'shoplist' : shoplist})
