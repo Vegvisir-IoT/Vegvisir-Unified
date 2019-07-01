bde86e1c70536b94be8ce03a1871513fd378acb9
Karan Newatia <kn348@cornell.edu>
2019-07-01 15:44:14 -0400

    fixed errors after logout

--- src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/VegvisirApplicationDelegatorImpl.java
+++ src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/VegvisirApplicationDelegatorImpl.java
@@ -1,13 +1,10 @@
 package com.vegvisir.app.tasklist;
 
-import android.util.Log;
-
+import com.vegvisir.app.tasklist.ui.login.LoginActivity;
 import com.vegvisir.pub_sub.TransactionID;
 import com.vegvisir.pub_sub.VegvisirApplicationDelegator;
 import com.vegvisir.pub_sub.VegvisirInstance;
 
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -39,128 +36,130 @@ public class VegvisirApplicationDelegatorImpl implements VegvisirApplicationDele
             TransactionID tx_id,
             Set<TransactionID> deps) {
 
-        String payloadString = new String(payload);
-        Log.i("appimpl",payloadString);
+          LoginActivity.delegator.applyTransaction(topics, payload, tx_id, deps);
 
-        int transactionType = Integer.parseInt(payloadString.substring(0,1));
-        String item = payloadString.substring(1);
-
-        Set<TransactionTuple> updatedSet = new HashSet<>();
-        Set<TransactionTuple> prevSets = MainActivity.dependencySets.get(item);
-        String deviceId = tx_id.getDeviceID();
-
-        if (prevSets != null) {
-            Iterator<TransactionTuple> itr = prevSets.iterator();
-            while (itr.hasNext()) {
-                TransactionTuple x = (TransactionTuple) ((Iterator) itr).next();
-
-                if (!deps.contains(x.transaction)) {
-
-                    updatedSet.add(x);
-                }
-            }
-        }
-
-
-
-        TransactionTuple t = new TransactionTuple(tx_id, transactionType);
-        updatedSet.add(t);
-        MainActivity.dependencySets.put(item, updatedSet);
-
-        MainActivity.latestTransactions.put(deviceId, tx_id);
-
-        for (TransactionID d : deps) {
-            MainActivity.topDeps.remove(d);
-        }
-        MainActivity.topDeps.add(tx_id);
-        HashSet<String> lowSet = new HashSet<>();
-        HashSet<String> mediumSet = new HashSet<>();
-        HashSet<String> highSet = new HashSet<>();
-        HashSet<String> removeSet = new HashSet<>();
-
-        for (TransactionID d : deps) {
-            if (MainActivity.fourPSets.containsKey(d)) {
-                lowSet.addAll(MainActivity.fourPSets.get(d).getLowSet());
-                mediumSet.addAll(MainActivity.fourPSets.get(d).getMediumSet());
-                highSet.addAll(MainActivity.fourPSets.get(d).getHighSet());
-                removeSet.addAll(MainActivity.fourPSets.get(d).getRemoveSet());
-            }
-        }
-
-        if (transactionType == 1) {
-            lowSet.add(item);
-            mediumSet.remove(item);
-            highSet.remove(item);
-            removeSet.remove(item);
-        }
-        else if (transactionType == 2) {
-            lowSet.remove(item);
-            mediumSet.add(item);
-            highSet.remove(item);
-            removeSet.remove(item);
-        }
-        else if (transactionType == 3) {
-            lowSet.remove(item);
-            mediumSet.remove(item);
-            highSet.add(item);
-            removeSet.remove(item);
-        }
-        else if (transactionType == 0){
-            lowSet.remove(item);
-            mediumSet.remove(item);
-            highSet.remove(item);
-            removeSet.add(item);
-        }
-
-        MainActivity.fourPSets.put(tx_id, new FourPSet(lowSet, mediumSet, highSet, removeSet));
-
-        HashSet<String> lowSetTop = new HashSet<>();
-        HashSet<String> mediumSetTop = new HashSet<>();
-        HashSet<String> highSetTop = new HashSet<>();
-        HashSet<String> removeSetTop = new HashSet<>();
-
-        for (TransactionID d : MainActivity.topDeps) {
-            if (MainActivity.fourPSets.containsKey(d)) {
-                lowSetTop.addAll(MainActivity.fourPSets.get(d).getLowSet());
-                mediumSetTop.addAll(MainActivity.fourPSets.get(d).getMediumSet());
-                highSetTop.addAll(MainActivity.fourPSets.get(d).getHighSet());
-                removeSetTop.addAll(MainActivity.fourPSets.get(d).getRemoveSet());
-            }
-        }
-
-        MainActivity.fourPSets.put(MainActivity.top, new FourPSet(lowSetTop, mediumSetTop, highSetTop, removeSetTop));
-
-        MainActivity.items.clear();
-        MainActivity.priorities.clear();
-
-        Set<String> newLowSet = lowSetTop;
-        newLowSet.removeAll(mediumSetTop);
-        newLowSet.removeAll(highSetTop);
-        newLowSet.removeAll(removeSetTop);
-
-        for(String lowItem: newLowSet) {
-            MainActivity.items.add(lowItem);
-            MainActivity.priorities.put(lowItem, MainActivity.Priority.Low);
-        }
-
-        Set<String> newMediumSet = mediumSetTop;
-        newMediumSet.removeAll(highSetTop);
-        newMediumSet.removeAll(removeSetTop);
-
-        for(String mediumItem: newMediumSet) {
-            MainActivity.items.add(mediumItem);
-            MainActivity.priorities.put(mediumItem, MainActivity.Priority.Medium);
-        }
-
-        Set<String> newHighSet = highSetTop;
-        newHighSet.removeAll(removeSetTop);
-
-        for(String highItem: newHighSet) {
-            MainActivity.items.add(highItem);
-            MainActivity.priorities.put(highItem, MainActivity.Priority.High);
-        }
-
-        MainActivity.notWitnessedTransactions.add(tx_id);
+//        String payloadString = new String(payload);
+//        Log.i("appimpl",payloadString);
+//
+//        int transactionType = Integer.parseInt(payloadString.substring(0,1));
+//        String item = payloadString.substring(1);
+//
+//        Set<TransactionTuple> updatedSet = new HashSet<>();
+//        Set<TransactionTuple> prevSets = MainActivity.dependencySets.get(item);
+//        String deviceId = tx_id.getDeviceID();
+//
+//        if (prevSets != null) {
+//            Iterator<TransactionTuple> itr = prevSets.iterator();
+//            while (itr.hasNext()) {
+//                TransactionTuple x = (TransactionTuple) ((Iterator) itr).next();
+//
+//                if (!deps.contains(x.transaction)) {
+//
+//                    updatedSet.add(x);
+//                }
+//            }
+//        }
+//
+//
+//
+//        TransactionTuple t = new TransactionTuple(tx_id, transactionType);
+//        updatedSet.add(t);
+//        MainActivity.dependencySets.put(item, updatedSet);
+//
+//        MainActivity.latestTransactions.put(deviceId, tx_id);
+//
+//        for (TransactionID d : deps) {
+//            MainActivity.topDeps.remove(d);
+//        }
+//        MainActivity.topDeps.add(tx_id);
+//        HashSet<String> lowSet = new HashSet<>();
+//        HashSet<String> mediumSet = new HashSet<>();
+//        HashSet<String> highSet = new HashSet<>();
+//        HashSet<String> removeSet = new HashSet<>();
+//
+//        for (TransactionID d : deps) {
+//            if (MainActivity.fourPSets.containsKey(d)) {
+//                lowSet.addAll(MainActivity.fourPSets.get(d).getLowSet());
+//                mediumSet.addAll(MainActivity.fourPSets.get(d).getMediumSet());
+//                highSet.addAll(MainActivity.fourPSets.get(d).getHighSet());
+//                removeSet.addAll(MainActivity.fourPSets.get(d).getRemoveSet());
+//            }
+//        }
+//
+//        if (transactionType == 1) {
+//            lowSet.add(item);
+//            mediumSet.remove(item);
+//            highSet.remove(item);
+//            removeSet.remove(item);
+//        }
+//        else if (transactionType == 2) {
+//            lowSet.remove(item);
+//            mediumSet.add(item);
+//            highSet.remove(item);
+//            removeSet.remove(item);
+//        }
+//        else if (transactionType == 3) {
+//            lowSet.remove(item);
+//            mediumSet.remove(item);
+//            highSet.add(item);
+//            removeSet.remove(item);
+//        }
+//        else if (transactionType == 0){
+//            lowSet.remove(item);
+//            mediumSet.remove(item);
+//            highSet.remove(item);
+//            removeSet.add(item);
+//        }
+//
+//        MainActivity.fourPSets.put(tx_id, new FourPSet(lowSet, mediumSet, highSet, removeSet));
+//
+//        HashSet<String> lowSetTop = new HashSet<>();
+//        HashSet<String> mediumSetTop = new HashSet<>();
+//        HashSet<String> highSetTop = new HashSet<>();
+//        HashSet<String> removeSetTop = new HashSet<>();
+//
+//        for (TransactionID d : MainActivity.topDeps) {
+//            if (MainActivity.fourPSets.containsKey(d)) {
+//                lowSetTop.addAll(MainActivity.fourPSets.get(d).getLowSet());
+//                mediumSetTop.addAll(MainActivity.fourPSets.get(d).getMediumSet());
+//                highSetTop.addAll(MainActivity.fourPSets.get(d).getHighSet());
+//                removeSetTop.addAll(MainActivity.fourPSets.get(d).getRemoveSet());
+//            }
+//        }
+//
+//        MainActivity.fourPSets.put(MainActivity.top, new FourPSet(lowSetTop, mediumSetTop, highSetTop, removeSetTop));
+//
+//        MainActivity.items.clear();
+//        MainActivity.priorities.clear();
+//
+//        Set<String> newLowSet = lowSetTop;
+//        newLowSet.removeAll(mediumSetTop);
+//        newLowSet.removeAll(highSetTop);
+//        newLowSet.removeAll(removeSetTop);
+//
+//        for(String lowItem: newLowSet) {
+//            MainActivity.items.add(lowItem);
+//            MainActivity.priorities.put(lowItem, MainActivity.Priority.Low);
+//        }
+//
+//        Set<String> newMediumSet = mediumSetTop;
+//        newMediumSet.removeAll(highSetTop);
+//        newMediumSet.removeAll(removeSetTop);
+//
+//        for(String mediumItem: newMediumSet) {
+//            MainActivity.items.add(mediumItem);
+//            MainActivity.priorities.put(mediumItem, MainActivity.Priority.Medium);
+//        }
+//
+//        Set<String> newHighSet = highSetTop;
+//        newHighSet.removeAll(removeSetTop);
+//
+//        for(String highItem: newHighSet) {
+//            MainActivity.items.add(highItem);
+//            MainActivity.priorities.put(highItem, MainActivity.Priority.High);
+//        }
+//
+//        MainActivity.notWitnessedTransactions.add(tx_id);
 
 
     }
