1fdb0b1ad9eafb7813c7902d4c4b17575d4b6b0c
Raheel Yanful <raheel@Raheel-Yanfuls-MacBook-Pro.local>
2019-06-30 20:19:28 -0400

    starting add, apply and login for users

--- src/pub_sub/python/ShoppingList/shoplist/views.py
+++ src/pub_sub/python/ShoppingList/shoplist/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
 
-from .models import Item, Person, ShoppingList
+from .models import Item, Person, ShoppingList, Transaction
+
 
 # Create your views here.
-def index(request):
 
+def login(request):
+    if(request.method == 'POST'):
+        userid = request.POST['userid']
+
+        user = auth.authenticate(username = userid)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:    
+            messages.info(request, 'invalid credentials')
+    else:
+        return render(request, 'login.html')
+def index(request):
     item1 = Item()
     item1.name = "apples"
     item1.isOn = True
@@ -22,8 +37,23 @@ def index(request):
     shoplist = ShoppingList()
     shoplist.items = [item1, item2]
     shoplist.people = [user1]
-    
     return render(request, 'index.html', {'shoplist' : shoplist})
 
+def add(request):
+    newTxn = Transaction()
+    newTxn.payload = str(request.POST['newitem'])
+
+    #push transaction to blockchain
+    #shoplist = shoplist + newitem
+    
+    apply(request) #because only 1 witness is necessary
+    return render(request, 'index.html')
+
+
 def apply(request):
-    return
+
+    txn = Transaction #valid transaction from blockchain
+    newItem = Item()
+    newItem.name = txn.payload
+
+    return render(request, 'index.html', {'shoplist' : shoplist})
