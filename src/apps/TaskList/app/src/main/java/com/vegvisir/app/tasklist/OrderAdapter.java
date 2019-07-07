3628ccdca1171a28d3291b5bc8cb44fa0fc359b5
Karan Newatia <kn348@cornell.edu>
2019-07-07 16:36:09 -0400

    changed background color of buttons

--- src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/OrderAdapter.java
+++ src/apps/TaskList/app/src/main/java/com/vegvisir/app/tasklist/OrderAdapter.java
@@ -55,13 +55,14 @@ public class OrderAdapter extends ArrayAdapter<String> {
         selected_item.setText(item);
         LoginActivity.Priority p = LoginActivity.priorities.get(item);
         selected_item.setTextColor( p.getAssociatedColor());
+        listItemView.setBackgroundColor(0);
 
 
         //OnClick listeners for all the buttons on the ListView Item
-        low_priority.setOnClickListener( e -> setTheTransaction( item, 1));
-        medium_priority.setOnClickListener( e -> setTheTransaction( item, 2));
-        high_priority.setOnClickListener( e -> setTheTransaction( item, 3));
-        delete_item.setOnClickListener( e -> setTheTransaction( item, 0));
+        low_priority.setOnClickListener( e -> setTheTransaction( item, 1, convertView));
+        medium_priority.setOnClickListener( e -> setTheTransaction( item, 2, convertView));
+        high_priority.setOnClickListener( e -> setTheTransaction( item, 3, convertView));
+        delete_item.setOnClickListener( e -> setTheTransaction( item, 0, convertView));
 
         return listItemView;
     }
@@ -71,7 +72,23 @@ public class OrderAdapter extends ArrayAdapter<String> {
      * @param item
      * @param myNumber
      */
-    public void setTheTransaction (String item, int myNumber ) {
+    public void setTheTransaction (String item, int myNumber, View v ) {
+        if (v != null){
+            if (myNumber == 1){
+                v.setBackgroundColor(this.context.getResources().getColor(R.color.LightGreen));
+            }
+            else if (myNumber == 2){
+                v.setBackgroundColor(this.context.getResources().getColor(R.color.LightSkyBlue));
+            }
+            else if (myNumber == 3){
+                v.setBackgroundColor(this.context.getResources().getColor(R.color.LightPink));
+            }
+            else{
+                v.setBackgroundColor(this.context.getResources().getColor(R.color.LightGrey));
+            }
+
+        }
+
         String payloadString = myNumber + item;
         byte[] payload = payloadString.getBytes();
         Set<String> topics = new HashSet<>(Arrays.asList( LoginActivity.topic));
