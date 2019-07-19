5f38f8f0ce24f9665e803d95cb0937e4c161a636
Raheel Yanful <raheel@dhcp-rhodes-997.eduroam.cornell.edu>
2019-07-19 18:49:10 -0400

    initial commit for 2pset
    able to add to it
    
    need to add removing functionality

--- src/pub_sub/python/ShoppingList/shoplist/urls.py
+++ src/pub_sub/python/ShoppingList/shoplist/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = "index"),
     path('add', views.add, name='add'),
+    path('remove', views.remove, name='remove'),
     #path('login', views.login, name='login')
 ]
