0436d7a85eb3aec63e83e2d550b5ee0a51f2236d
Raheel Yanful <raheel@dhcp-rhodes-2445.eduroam.cornell.edu>
2019-07-22 16:04:08 -0400

    initial commit

--- src/Vegvisir/vegvisir/blockchain/python/ShoppingList/shoplist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = "index"),
-    path('add', views.add, name='add'),
-    path('remove', views.remove, name='remove'),
-    #path('login', views.login, name='login')
-]
