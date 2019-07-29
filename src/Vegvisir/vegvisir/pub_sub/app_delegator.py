1a71d6ea7b2b3067d2895ff3f8da82f883071bf7
Raheel Yanful <raheel@dhcp-rhodes-2445.eduroam.cornell.edu>
2019-07-29 19:08:46 -0400

    for stephen

---
+++ src/Vegvisir/vegvisir/pub_sub/app_delegator.py
@@ -0,0 +1,18 @@
+from vegvisir.blockchain.block import TransactionId
+from vegvisir.app.ShoppingList.shoplist.models import app
+
+class VirtualVegvisirAppDelegator(VegvisirAppDelegator):
+
+    """
+        :param instance: A VegvisirInstance for the application to use.
+    """
+    def __init__(self, instance, context):
+         super().__init__(instance, context)
+
+    def applyTransaction(topics : set(), payload : bytes, tx_id : TransactionId, deps : set()):
+
+        item = payload[1:]
+        operation = payload[0]
+        app.TwoP.updateSet(item, operation, app.TwoP)
+        return
+
