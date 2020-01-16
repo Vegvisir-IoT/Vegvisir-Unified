49812a939f00a693f05e2a7a5576062ea3d70dcf
Xinwen Wang <xw467@cornell.edu>
2020-01-16 11:09:00 -0500

    remove python

--- src/pub_sub/python/ShoppingList/shoplist/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Item, Person, Transaction
-# Register your models here.
-admin.site.register(Item)
-admin.site.register(Person)
-admin.site.register(Transaction)
